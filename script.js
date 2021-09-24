e879dae484034ff3d78db0df7cf1d338ed308d83
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-09-23 22:46:55 -0700

    js updates

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateBtn = document.getElementById('generate');
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
+
 function writePassword() { 
-  var password = generatePassword();
+  console.log("cheese");
+  var password = passwordPrompt();
   var passwordText = document.querySelector("#password");
   
 
@@ -12,38 +30,76 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-var charLength = []
-var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz"
-var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "123456789"
-var symbolsc= "!@#$%^&*><?][}{"
-
-var generatePassword = function () {
-  var passwordLength = prompt ("Choose between 8 - 128 characters");
-
-  function checkInput(passwordLength !== "8"); {
-    return;
+var passwordPrompt = function () {
+  let length = prompt ("Choose between 8 - 128 characters");
+    if (length < 8) {
+    alert("Minimum characters: 8, assinging value as 8");
+    length = 8;
+    }
+    if (length > 128) {
+      alert("Maximum characters: 128, assinging value as 128");
+      length = 128;
+      }
+  let lower = confirm ("Do you want lower case characters?");
+    
+  let upper = confirm ("Do you want upper case characters?");
+   
+  let number = confirm ("Do you want numeric characters?");
+    
+  let symbol = confirm ("Do you want special characters?");
+    
+  // If user does not choose any character type above, default to lower case
+  if (!lower && !upper && !number && !symbol) {
+    alert("Must choose at least one character type, default to lower case");
+    lower = true;
   }
-  function checkInput(passwordLength <== "8" || >== "128");
-  console.log (passwordLength)
-  var lowerCase = confirm ("Do you want lower case characters?"); 
+    
+    
+    return generatePassword(lower, upper, number, symbol, length);
+    
 }
 
-  var charIndex = Math.floor(Math.random() * charLength.length);
+function generatePassword(lower, upper, number, symbol, length) {
+ let generatedPassword = '';
 
-  var lowerCase = confirm ("Do you want lower case characters?"); 
-  var upperCase = confirm ("Do you want upper case characters?");
-  var numericCase = confirm ("Do you want numeric characters?");
-  var symbolCase = confirm ("Do you want special characters?");
+const typesCount = lower + upper + number + symbol;
 
+const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+  (item => Object.values(item)[0]
+  );
+  
+  console.log('typesArr: ', typesArr);
 
+  if(typesCount == 0) {
+    return '';
+  }
 
-if (lowercase)
+  for(let i=0; i<length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      console.log('funcName', funcName);
+
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+  console.log(finalPassword);
+  return finalPassword;
 
 }
-
+    function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol(){
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 
